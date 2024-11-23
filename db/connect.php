d8c206b143c313844dd071608790b4ee846d22ab
Faith Ruto <faith.ruto@ashesi.edu.gh>
2024-11-23 04:53:04 +0000

    Update connect.php

--- db/connect.php
+++ db/connect.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "faith.ruto";       
 $password = "Fr@17005566";      
-$database = "casaconnect"; 
+$database = "webtech_fall2024_faith_ruto"; 
 
 // Create a new database connection
 $conn = new mysqli($servername, $username, $password, $database);
